46e1d74545e7cc650229b999bec19187c51ef282
Robin Ilyk <contact@robko.cz>
2025-03-10 16:44:25 +0100

    django login register

---
+++ backend/accounts/urls.py
@@ -0,0 +1,6 @@
+from accounts import views
+from django.urls import path
+
+urlpatterns = [
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+]
--- backend/vpsi_backend/settings.py
+++ backend/vpsi_backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'vpsi_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home" 
